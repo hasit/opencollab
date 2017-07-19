9d0fd4f89ccc8320b1b53950152612864b148493
hasit <hasit@gmail.com>
2017-07-19 09:38:39 -0700

    Move all nav links to home.go

--- actions/home.go
+++ actions/home.go
@@ -4,5 +4,15 @@ import "github.com/gobuffalo/buffalo"
 
 // HomeHandler is a default handler to serve up a home page.
 func HomeHandler(c buffalo.Context) error {
-	return c.Render(200, r.HTML("projects/index.html"))
+	return c.Render(200, r.HTML("index.html"))
+}
+
+// AboutHandler is a default handler to serve the about page.
+func AboutHandler(c buffalo.Context) error {
+	return c.Render(200, r.HTML("about.html"))
+}
+
+// LoginHandler is the handler to serve up the login page
+func LoginHandler(c buffalo.Context) error {
+	return c.Render(200, r.HTML("login.html"))
 }
--- actions/login.go
+++
@@ -1,8 +0,0 @@
-package actions
-
-import "github.com/gobuffalo/buffalo"
-
-// LoginHandler is the handler to serve up the login page
-func LoginHandler(c buffalo.Context) error {
-	return c.Render(200, r.HTML("login.html"))
-}
