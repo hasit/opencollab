0006766b1185f7229d17734b0081471ca81bdffe
hasit <hasit@gmail.com>
2017-07-22 12:05:53 -0700

    List projects by default on the home page

--- actions/home.go
+++ actions/home.go
@@ -4,7 +4,7 @@ import "github.com/gobuffalo/buffalo"
 
 // HomeHandler is a default handler to serve up a home page.
 func HomeHandler(c buffalo.Context) error {
-	return c.Render(200, r.HTML("index.html"))
+	return c.Render(200, r.HTML("projects/index.html"))
 }
 
 // AboutHandler is a default handler to serve the about page.
